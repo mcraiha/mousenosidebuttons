81e36c9d8eccd7eb2383fbf8e245e73fd7db575f
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2026-02-04 09:15:22 +0200

    Fix weight sorting
    Just numbers

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
-export function sortTable(tableId: string, columnIndex) {
+export function sortTable(tableId: string, columnName: string, columnIndex: any) {
 
     const table = document.getElementById(tableId)! as HTMLTableElement;
     const rows = table.tBodies[0].rows;
 
     let arr = Array.from(rows);
     let sortedRows = arr.sort((a, b) => {
-        let cellA = a.cells[columnIndex].innerText;
-        let cellB = b.cells[columnIndex].innerText;
-        return cellA.localeCompare(cellB);
+        if (columnName === "weight" || columnName === "weightdis")
+        {
+            let cellA = a.cells[columnIndex].innerText.slice(0, -1);
+            let cellB = b.cells[columnIndex].innerText.slice(0, -1);
+            return Number.parseInt(cellA, 10) - Number.parseInt(cellB, 10);
+        }
+        else
+        {
+            let cellA = a.cells[columnIndex].innerText;
+            let cellB = b.cells[columnIndex].innerText;
+            return cellA.localeCompare(cellB);
+        }
     });
 
     sortedRows.forEach(row => table.tBodies[0].appendChild(row));
@@ -18,12 +27,12 @@ const sortersDiscontinued: readonly string[] = ["makerdis", "modeldis", "buttona
 
 sortersCurrent.forEach((value, index) => {
     document.getElementById(value)!.addEventListener("click", function() {
-      sortTable("current", index)
+      sortTable("current", value, index)
     });
 });
 
 sortersDiscontinued.forEach((value, index) => {
     document.getElementById(value)!.addEventListener("click", function() {
-      sortTable("discontinued", index)
+      sortTable("discontinued", value, index)
     });
 });
